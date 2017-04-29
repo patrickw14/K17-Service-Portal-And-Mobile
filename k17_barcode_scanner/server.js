ccbb1e43f7d5d545071543af7dd9b311ae940c84
Patrick Wilson <patrick.wilson.14@gmail.com>
2017-04-29 15:58:17 -0700

    Delete server.js

--- k17_barcode_scanner/server.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  /* populate the 'data' object */
-  /* e.g., data.table = $sp.getValue('table'); */	
-	
-	if (input && input.action == "get_asset") {
-		data.assetID = input.barcode;
-		return;
-	}
-})();
