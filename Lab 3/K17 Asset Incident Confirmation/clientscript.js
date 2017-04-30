cd1f6d7c2a3b132edbd6d01b7aca8ff7ff47b208
Patrick Wilson <patrick.wilson.14@gmail.com>
2017-04-30 14:45:09 -0700

    Create clientscript.js

---
+++ Lab 3/K17 Asset Incident Confirmation/clientscript.js
@@ -0,0 +1,28 @@
+function(cabrillo) {
+  /* widget controller */
+  var c = this;
+	
+	if (!c.data.asset.questions.length) {
+		cabrillo.viewLayout.setBottomButtons([{
+			title: 'Report Incident',
+			enabled: true,
+			backgroundColor: '#2d86d5',
+			textColor: '#FFFFFF'
+		}], function(e) {
+			c.reportIncident();
+		});	
+	}
+
+	c.reportIncident = function(reason, index) {
+		c.selectedIndex = index;
+		c.server.get({
+			action: "report_incident",
+			reason: reason,
+			assetName: c.data.asset.name
+		}).then(function() {
+			cabrillo.modal.dismissModal({
+				success: true
+			});
+		});
+	}
+}
