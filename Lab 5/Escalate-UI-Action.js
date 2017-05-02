ea89b50831e07d2ea7fadc5cd3337f208c217139
Patrick Wilson <patrick.wilson.14@gmail.com>
2017-05-02 10:53:20 -0700

    Update Escalate-UI-Action.js

--- Lab 5/Escalate-UI-Action.js
+++ Lab 5/Escalate-UI-Action.js
@@ -2,7 +2,7 @@ current.impact = '1';
 current.urgency = '1';
 current.update();
 
-var isCoffeeIncident = current.short_description.includes('coffee');
+var isCoffeeIncident = current.short_description.includes('Coffee');
 
 if (isCoffeeIncident) {
 	action.setRedirectURL('/$sp.do?id=k17_escalate_coffee_incident');
