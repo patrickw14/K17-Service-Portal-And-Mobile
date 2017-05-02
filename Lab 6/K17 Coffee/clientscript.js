bddd65648971f2eadccf9a2a403a756b81c025dc
Patrick Wilson <patrick.wilson.14@gmail.com>
2017-05-02 10:54:27 -0700

    Create clientscript.js

---
+++ Lab 6/K17 Coffee/clientscript.js
@@ -0,0 +1,36 @@
+function(cabrillo) {
+	/* widget controller */
+	var c = this;
+
+	c.getLocation = function() {
+		cabrillo.viewLayout.showSpinner();
+		cabrillo.geolocation.getCurrentLocation().then(function(response) {
+			c.server.get({
+				action: "get_address",
+				lat: response.coordinate.latitude,
+				lng: response.coordinate.longitude
+			}).then(function(response) {
+				cabrillo.viewLayout.hideSpinner();
+				c.addressList = response.data.addressList.filter(function(address) {
+					return address.types.includes("street_address");
+				});
+
+				c.loaded = true;
+			})
+		});	
+	}
+
+	c.selectAddress = function(address, index) {
+		c.selectedIndex = index;
+
+		cabrillo.viewLayout.setBottomButtons([{
+			title: 'Order Coffee',
+			enabled: true,
+			backgroundColor: '#2d86d5',
+			textColor: '#FFFFFF'
+		}], function(e) {
+			alert("Ordered!");
+			cabrillo.navigation.goBack();
+		});
+	}
+}
